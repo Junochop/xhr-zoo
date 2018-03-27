cfe65260f3068b0f506c53395bd4b7fc3c69e089
Juno Lee <lejuno80@gmail.com>
2018-03-27 18:47:26 -0500

    updated card

--- main.js
+++ main.js
@@ -11,12 +11,11 @@ const builddomString = (animalArray) => {
     domString += `<div class="card">`;
     domString += `<h1>${animalArray[i].name}</h1>`;
     domString += `<h3>${animalArray[i].number}</h1>`;
-    domString += `<img class="animal-image" src=${animalArray[i].imageUrl} width="350" height="250">`;
+    domString += `<img class="animal-image" src=${animalArray[i].imageUrl} >`;
     domString +=  `<div class="button-container">`;
     domString +=    `<button class="card-button">Escape</button>`;
     domString +=  `</div>`;
     domString += `</div>`;
-
 
     };
 
